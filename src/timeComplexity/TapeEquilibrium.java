c71029ce47eaa0b0296ccba0284206f15e918a35
YeshayahuBennun <ybennun@yahoo.com>
2021-09-20 00:16:29 +0300

    commit#2 - TapeEquilibrium solution

--- src/timeComplexity/TapeEquilibrium.java
+++ src/timeComplexity/TapeEquilibrium.java
@@ -7,6 +7,23 @@ package timeComplexity;
  * @version 1.0.0
  * @since 19/09/2021
  */
+/*A non-empty zero-indexed array A consisting of N integers is given. Array A represents numbers on a tape.
+Any integer P, such that 0 < P < N, splits this tape into two non−empty parts: A\[0], A\[1], …, A\[P − 1] and A\[P], A\[P + 1], …, A\[N − 1].
+The difference between the two parts is the value of: |(A\[0] + A\[1] + … + A\[P − 1]) − (A\[P] + A\[P + 1] + … + A\[N − 1])|
+In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+
+Write a function that, given a non-empty zero-indexed array A of N integers, returns the minimal difference that can be achieved.
+
+Example: A[0] = 3 A[1] = 1 A[2] = 2 A[3] = 4 A[4] = 3
+We can split this tape in four places:
+  P = 1, difference = |3 − 10| = 7
+  P = 2, difference = |4 − 9| = 5
+  P = 3, difference = |6 − 7| = 1
+  P = 4, difference = |10 − 3| = 7
+In this case I would return 1 as it is the smallest difference.
+
+N is an int, range [2..100,000]; each element of A is an int, range [−1,000..1,000]. It needs to be O(n) time complexity,
+*/
 public class TapeEquilibrium {
     public int solution(int[]A){
         int leftSum=A[0];
